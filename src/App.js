f740245b4811951d1f82753518bd5aa42f0e9e06
Techiediaries <techiediaries9@gmail.com>
2019-09-23 18:42:34 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+
+const GET_POKEMON_INFO = gql`
+{
+    pokemons(first: 150) {
+      id
+      number
+      name,
+      image,
+      evolutions {
+        id,
+        number,
+        name,
+        image
+      }
+    }
+  }
+`
 function App() {
+  const { data, loading, error } = useQuery(GET_POKEMON_INFO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>
+        Pokémons
+  
+</h1>
+
+      <p>
+        <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">The Pokémon franchise</a> revolves around 832 fictional species of collectible monsters, each having unique designs and skills. Conceived by Satoshi Tajiri in early 1989, Pokémon are creatures that inhabit the fictional Pokémon World.
+        This is the list of the first 150 Pokémon as they appear in Pokémon Stadium, starting with Bulbasaur in the top left corner and ending with Mewtwo in the bottom right corner.
+        
+</p>
+      <div className="container">
+
+        {data && data.pokemons &&
+          data.pokemons.map((pokemon, index) => (
+
+            <div key={index} className="card">
+
+              <img src={pokemon.image} />
+              <div class="card-body">
+                <h3>{pokemon.name}</h3>
+                <p>
+                  {pokemon.evolutions && (pokemon.evolutions.length !== 0) && <p> Evolutions:
+          
+{pokemon.evolutions.map((e, indx) => {
+
+                      return <p key={indx}> {e.name} </p>
+                    })}
+                  </p>}
+
+
+
+                </p>
+              </div>
+            </div>
+
+          ))}
+      </div>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
